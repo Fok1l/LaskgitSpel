aee8f548e7fdab69dff2ab9f18d3f6f946ea1ac8
Felix Sporrong Carlander <felix.sporrongcarla@learnet.se>
2024-03-15 09:07:20 +0100

    aswdfawfs
    awdaeg

--- Assets/Scripts/Felix/NoteSystem.cs
+++ Assets/Scripts/Felix/NoteSystem.cs
@@ -6,24 +6,44 @@ using UnityEngine.UI;
 public class NoteSystem : MonoBehaviour
 {
     PlayerMove player;
-    public Image Note1;
-    public bool NoteObject1 = false;
 
-    [SerializeField] GameObject Paper1;
+    public Image[] noteImages; // Array of note UI Images
+    public bool[] noteObjects; // Array of boolean flags for each note (Helps in enbaling/disabling ui and papers)
 
-    // Start is called before the first frame update
+    [SerializeField] GameObject[] papers; // Array of paper GameObjects
+
     void Start()
     {
         player = FindObjectOfType<PlayerMove>();
+
+        // Disable all note images initially
+        foreach (Image noteImage in noteImages)
+        {
+            noteImage.enabled = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            NoteObject1 = true;
-            Note1.enabled = true;
-            Destroy(gameObject);
+            // Find the index of the first available note slot
+            int noteIndex = -1;
+            for (int i = 0; i < noteObjects.Length; i++)
+            {
+                if (!noteObjects[i])
+                {
+                    noteIndex = i;
+                    break;
+                }
+            }
+
+            if (noteIndex != -1)
+            {
+                noteObjects[noteIndex] = true;
+                noteImages[noteIndex].enabled = true;
+                Destroy(gameObject);
+            }
         }
     }
 }
