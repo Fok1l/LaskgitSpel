d76ac1f70187fb4ffabfe7b4462d87a3abd1898e
Leon Bille <leon.bille@elev.ga.lbs.se>
2024-03-22 09:38:13 +0100

    Zap-Game
    Zap-Game completion

--- Assets/Scripts/Leon/KeyScript.cs
+++ Assets/Scripts/Leon/KeyScript.cs
@@ -1,6 +1,7 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class KeyScript : MonoBehaviour
 {
@@ -12,7 +13,8 @@ public class KeyScript : MonoBehaviour
     SceneLoader sceneLoader;
 
     public bool isAlive = true;
-    public Sprite[] spritePicture;
+
+    public List<GameObject> blixtar;
 
     private float moveVertical;
     private float moveHorizontal;
@@ -57,7 +59,6 @@ public class KeyScript : MonoBehaviour
         yield return new WaitForSeconds(deathCooldown);
 
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-
         SceneManager.LoadScene(currentSceneIndex); // Reloading the scene
     }
 
@@ -83,8 +84,9 @@ public class KeyScript : MonoBehaviour
 
     void randomSpriteTrigger() // To spawn a random lightning when dying
     {
-        random = Random.Range(0, spritePicture.Length);
-        GetComponent<SpriteRenderer>().sprite = spritePicture[random];
+        random = Random.Range(0, blixtar.Count);
+        blixtar[random].SetActive(true);
+        blixtar[random].GetComponent<Transform>().position = transform.position + transform.up;
     }
 }
 
