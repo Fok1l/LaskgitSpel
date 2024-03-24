1aaa5532a031e06ef2e4ce4aac36548d127a2c45
Felix Sporrong Carlander <felix.sporrongcarla@learnet.se>
2024-03-24 12:51:16 +0100

    aswdawd
    QSDwdw

--- Assets/Scripts/Felix/NoteSystem.cs
+++ Assets/Scripts/Felix/NoteSystem.cs
@@ -35,7 +35,6 @@ public class NoteSystem : MonoBehaviour
             {
                 if (papers[i] == gameObject)
                 {
-                    AudioManager.
                     paperIndex = i;
                     break;
                 }
