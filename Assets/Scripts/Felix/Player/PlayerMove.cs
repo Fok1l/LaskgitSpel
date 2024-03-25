5adc5d04a584692540fd85df7cc3c94f0f9aba1a
Felix Sporrong Carlander <felix.sporrongcarla@learnet.se>
2024-03-25 17:33:36 +0100

    Player Fix
    dwda

--- Assets/Scripts/Felix/Player/PlayerMove.cs
+++ Assets/Scripts/Felix/Player/PlayerMove.cs
@@ -8,32 +8,32 @@ public class PlayerMove : MonoBehaviour
     private float vertical;
     [SerializeField] private float speed = 8f;
 
-    [SerializeField] GameObject flashLightObject;
-    public bool usingFlashLight = false;
+   // [SerializeField] GameObject flashLightObject;
+   // public bool usingFlashLight = false;
 
     private Vector2 moveInput;
     private Rigidbody2D thisRigidBody;
     private Transform characterTransform;
 
-    Animator myAnimator;
-    PlayerAnimations playerAnimations;
+   // Animator myAnimator;
+   // PlayerAnimations playerAnimations;
 
 
     void Start()
     {
-        myAnimator = GetComponent<Animator>();
+     //   myAnimator = GetComponent<Animator>();
         thisRigidBody = GetComponent<Rigidbody2D>();
-        playerAnimations = GetComponent<PlayerAnimations>();
+     //   playerAnimations = GetComponent<PlayerAnimations>();
         characterTransform = transform;
     }
 
     void Update()
     {
-        playerAnimations.WalkingAnim();
+      //  playerAnimations.WalkingAnim();
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput = SnapToYX(moveInput);
-        AccessInventory();
+        //AccessInventory();
     }
 
     private void FixedUpdate()
@@ -58,23 +58,23 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    void AccessInventory()
-    {
-        if (!usingFlashLight && Input.GetKeyUp(KeyCode.R))
-        {
+    //void AccessInventory()
+   // {
+   //     if (!usingFlashLight && Input.GetKeyUp(KeyCode.R))
+   //     {
             //unlitFlashLight.gameObject.SetActive(false);
-            usingFlashLight = true;
+     //       usingFlashLight = true;
             //litFlashLight.gameObject.SetActive(true);
-            flashLightObject.SetActive(true);
-        }
-        else if (usingFlashLight == true && Input.GetKeyUp(KeyCode.R))
-        {
+    //        flashLightObject.SetActive(true);
+    //    }
+    //    else if (usingFlashLight == true && Input.GetKeyUp(KeyCode.R))
+     //   {
             //unlitFlashLight.gameObject.SetActive(true);
-            usingFlashLight = false;
+      //      usingFlashLight = false;
             //litFlashLight.gameObject.SetActive(false);
-            flashLightObject.SetActive(false);
-        }
-    }
+        //    flashLightObject.SetActive(false);
+    //    }
+    //}
 
     private Vector2 SnapToYX(Vector2 input)
     {
