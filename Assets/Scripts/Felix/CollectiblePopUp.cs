00cfc12695dbc1ee26f220f5ff6e633159dcb4da
Felix Sporrong Carlander <felix.sporrongcarla@learnet.se>
2024-04-19 09:14:00 +0200

    QS

---
+++ Assets/Scripts/Felix/CollectiblePopUp.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectiblePopUp : MonoBehaviour
+{
+    SaveTheBladeBool BladeBool;
+    bool SquareCollected = false;
+}
