5f037a7bc1d5d32f3ce4934cbcd52cf3119d490a
Felix Sporrong Carlander <felix.sporrongcarla@learnet.se>
2024-05-21 10:09:33 +0200

    d

---
+++ Assets/Scripts/Felix/FakeTutorial.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeTutorial : MonoBehaviour
+{
+    [SerializeField] Canvas FakeTutorial_Canvas;
+
+
+    private void Start()
+    {
+        FakeTutorial_Canvas.enabled = false;
+    }
+
+    public void ShowFake()
+    {
+        wait();
+        
+
+    }
+    IEnumerator wait()
+    {
+        FakeTutorial_Canvas.enabled = true;
+        yield return new WaitForSecondsRealtime(2);
+        FakeTutorial_Canvas.enabled = false;
+    }
+}
